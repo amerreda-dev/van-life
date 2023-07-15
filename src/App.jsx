632c76a71ffb8104ee980118077c41d17e778577
amerreda-dev <amer.reda193@gmail.com>
2023-07-16 01:38:37 +0300

    added header and layout using Outlet , and added Vandetails using useParams()

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,19 @@ import '../server'
 import About from '../Pages/About.jsx'
 import Vans from '../Pages/Vans'
 import VanDetail from '../Pages/VanDetail'
+import Layout from '../components/Layout'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <header>
-          <Link className="site-logo" to="/">
-            #VanLife
-          </Link>
-          <nav>
-            <Link to="/about">About</Link>
-            <Link to="/vans">Vans</Link>
-          </nav>
-        </header>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/vans" element={<Vans />} />
+            <Route path="/vans/:id" element={<VanDetail />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
